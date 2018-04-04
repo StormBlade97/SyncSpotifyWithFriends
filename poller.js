95329166abf11a2eb53182f65599237e6e2f0581
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-04-05 00:37:06 +0300

    MVP of the project. Can sync now

---
+++ poller.js
@@ -0,0 +1,30 @@
+const store = require('./userStore')
+const SpotifyUserApi = require('./spotifyApi')
+
+module.exports = () => {
+
+    let hostPaused;
+
+    const pollHostPlayerStatus = async () => {
+        if (store.getUserStore().length < 1) return;
+        const host = store.getHost()
+        const API = new SpotifyUserApi(host.accessToken, host.refreshToken)
+        const player = await API.getPlayer()
+        // TODO: might get into race condition
+        const hostProgress = player.progress_ms
+        const hostTrackUri = player.item.uri
+        
+        hostPaused = player.is_playing
+
+        store.getUserStore().forEach(async user => {
+            if (user === store.getHost()) return // if user is HOST, no-op
+            const API = new SpotifyUserApi(user.accessToken, user.refreshToken)
+            const userPlayer = await API.getPlayer()
+            const userProgress = userPlayer.progress_ms
+            const userTrackUri = userPlayer.item.uri
+            if (Math.abs(userProgress - hostProgress) > 200 || userTrackUri !== hostTrackUri) await API.syncTrack(hostTrackUri, hostProgress + 75)
+        })
+    }
+
+    setInterval(pollHostPlayerStatus, 3000)
+}
---
+++ routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router()
+const fetch = require('node-fetch')
+const spotify = require('./spotifyApi')
+const userStore = require('./userStore')
+const URLSearchParams = require('url').URLSearchParams
+
+const REDIRECT_URI = 'http://857b83d7.ngrok.io/loggedin'
+router.get('/', (req, res) => {
+    res.send("Christian is a poopoo head")
+})
+router.get('/login', (req, res) => {
+    const spotifyScopes = ['user-read-playback-state', 'user-read-currently-playing', 'user-modify-playback-state', 'user-read-private', 'user-read-email', 'user-read-birthdate']
+    const spotifyAuthURL = 'https://accounts.spotify.com/authorize' + 
+        '?response_type=code' + 
+        '&client_id=' + process.env.CLIENT_ID +
+        '&scope=' + spotifyScopes.join("+") +
+        '&redirect_uri=' + REDIRECT_URI;
+    
+    res.redirect(spotifyAuthURL)
+})
+
+router.get('/loggedin', async (req, res) => {
+    console.log("HELLOS")
+    if (req.query.error === 'access_denied') {
+        res.send('Access denied') // placeholder
+        return;
+    }
+    // now get access token
+    try {
+        const params = new URLSearchParams({
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: REDIRECT_URI,
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET
+        })
+        const responseJson = await fetch('https://accounts.spotify.com/api/token?' + params.toString(), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            }
+        })
+        const responseData = await responseJson.json()
+        userStore.createUser(responseData.access_token, responseData.refresh_token) // TODO worry about expires_in
+    }
+    catch (e) {
+        console.error(e)
+    }
+    res.redirect("/")
+})
+
+router.get('/logout', async (req, res) => {
+    // await userStore.createUser(req.query.code)
+    // res.send("okay!")
+    // res.redirect("/")
+})
+
+module.exports = router
