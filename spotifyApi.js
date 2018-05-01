b7063c0cac755c417fa8cc291c6bcc7009d4e257
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-05-01 19:45:43 +0300

    Prettier, basic apps

--- spotifyApi.js
+++ spotifyApi.js
@@ -1,32 +1,31 @@
 // const SpotifyWebApi = require('spotify-web-api-node')
-// const myOathToken = "BQA90wvstDly0qT_pnh2FxpnKrEvfKseezQM4McN2GYMI2IZcsCqBs1a0iYCHgMOmOenY92b7xds"
 // const spotify = new SpotifyWebApi({
 //     clientId: process.env.CLIENT_ID,
 //     clientSecret: process.env.CLIENT_SECRET,
-//     redirectUri: 'http://b04dc017.ngrok.io/loggedin'
+//     redirectUri: 'http://u.ngrok.io/loggedin'
 // })
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 class SpotifyUserAPI {
     constructor(accessToken, refreshToken) {
-        this.ACCESS_TOKEN = accessToken
-        this.REFRESH_TOKEN = refreshToken
-        this.HEADERS = { 
+        this.ACCESS_TOKEN = accessToken;
+        this.REFRESH_TOKEN = refreshToken;
+        this.HEADERS = {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        }
+            'Authorization': `Bearer ${accessToken}`,
+        };
     }
 
     async getProfile() {
-        const data = await fetch('https://api.spotify.com/v1/me', { headers: this.HEADERS })
-        return data.json()
+        const data = await fetch('https://api.spotify.com/v1/me', { headers: this.HEADERS });
+        return data.json();
     }
 
     async getPlayer() {
-        const data = await fetch('https://api.spotify.com/v1/me/player', { headers: this.HEADERS })
-        return data.json()
+        const data = await fetch('https://api.spotify.com/v1/me/player', { headers: this.HEADERS });
+        return data.json();
     }
 
     async syncTrack(trackUri, position_ms) {
@@ -35,16 +34,15 @@ class SpotifyUserAPI {
             headers: this.HEADERS,
             method: 'PUT',
             body: JSON.stringify({
-                uris: [trackUri]
-            })
-        })        
+                uris: [trackUri],
+            }),
+        });
         // sync time
         await fetch(`https://api.spotify.com/v1/me/player/seek?position_ms=${position_ms}`, {
             headers: this.HEADERS,
-            method: 'PUT'
-        })
+            method: 'PUT',
+        });
     }
 }
 
-module.exports = SpotifyUserAPI
-
+module.exports = SpotifyUserAPI;
